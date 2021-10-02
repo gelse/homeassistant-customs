f766969f71f65faa6514f752fb0a8cbb17eeda43
gelse <gitea@gelse.net>
2021-10-02 11:42:28 +0200

    finished simplest config flow

--- benq_projector/config_flow.py
+++ benq_projector/config_flow.py
@@ -10,8 +10,11 @@ class BenqProjectorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input: Optional[dict[str, Any]] = None) -> data_entry_flow.FlowResult:
 
         if user_input is not None:
-            # second step, the user input is filled here. validate the data, like connect the serial.
-            # if something is wrong, return dictionary with errors, key is the field name; "base" is special key for error without field.
+            return self.async_create_entry(
+                title="Benq projector configuration",
+                data=user_input
+            )
+
             # multi-step: return await self.async_step_second() with async_step_second signature like this one
             # finishing config:
             # return self.async_create_entry(
@@ -21,13 +24,20 @@ class BenqProjectorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             #   })
             # abort config:
             # return self.async_abort(reason="not_supported")
-            pass
 
-        data_schema = {
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({
             vol.Required("socket"): socket,
             vol.Optional("name", default="Benq Projector"): str,
-            vol.Optional("baudrate", default=9600): int,
-            vol.Optional("timeout", default=1): int
-        }
+            vol.Optional("baudrate", default=9600): vol.In([2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200]),
+            vol.Optional("timeout", default=1): vol.All(int, vol.Range(1, 10))
+        }))
 
-        return self.async_show_form(step_id="user", data_schema=vol.Schema(data_schema))
+#
+#    async def async_step_commands1(self, user_input: Optional[dict[str, Any]] = None) -> data_entry_flow.FlowResult:
+#        if user_input is not None:
+#            pass
+#        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
+#
+#    async def async_step_commands2(self, user_input: Optional[dict[str, Any]] = None) -> data_entry_flow.FlowResult:
+#        if user_input is not None:
+#            return self.async_create_entry()
